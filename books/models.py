7a2fafb597dcd5c846a8efb7be28bcaaea4de941
rambus2006 <bangminseo50@gmail.com>
2025-04-25 14:28:14 +0900

    update : 폼 추가함, 모델 추가함, 뷰스도 추가함, 시리얼은 홍배가 최종 수정해 줄 거임

--- books/models.py
+++ books/models.py
@@ -4,19 +4,15 @@ from django.contrib.auth import get_user_model
 User = get_user_model()
 
 class Book(models.Model):
+    category = models.ForeignKey("Category", on_delete=models.CASCADE, default=1)
     title = models.CharField("Title", max_length=200)
     description = models.TextField("Description")
-    customer_review_rank = models.FloatField("Customer review rank", null=True, blank=True)
+    isbn = models.CharField("Isbn", max_length=200)
+    cover = models.URLField("Cover", max_length=500)
+    publisher = models.CharField("Publisher", max_length=200)
+    pub_date = models.DateTimeField()
     author = models.CharField("Author", max_length=100)
-    cover_image = models.ImageField("Cover image", upload_to='books/', blank=True, null=True)
-
-    # AI 생성 정보
-    author_profile_image = models.ImageField("작가 프로필 이미지", upload_to='authors/', blank=True, null=True)
-    author_info = models.TextField("작가 정보 (AI 생성)", blank=True)
-    author_works = models.TextField("작가 대표작 목록 (AI 생성)", blank=True)
-
-    # gTTS 음성 파일
-    audio_file = models.FileField("Voice File", upload_to='tts/', blank=True, null=True)
+    customer_review_rank = models.FloatField("Customer review rank", null=True, blank=True)
 
     def __str__(self):
         return self.title
@@ -27,11 +23,25 @@ class Thread(models.Model):
     book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name='threads', default=1)
     title = models.CharField(max_length=100)
     content = models.TextField(null=True, blank=True)  # 선택값 허용
-    read_date = models.DateField(default=timezone.now)
+    reading_date = models.DateField(default=timezone.now)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    cover_image = models.ImageField(upload_to='thread_covers/', blank=True, null=True)
-    likes = models.ManyToManyField(User, related_name='liked_threads', blank=True)
+    
+    def __str__(self):
+        return self.title
 
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    thread = models.ForeignKey("thread", on_delete=models.CASCADE, related_name='comment', default=1)
+    content = models.TextField(null=True, blank=True)  # 선택값 허용
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True, editable=False)
+    
     def __str__(self):
         return self.title
